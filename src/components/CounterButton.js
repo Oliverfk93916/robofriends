a2b22af39ea1c2b55d30d38a971c118686b366d1
little_wing <oliverfk93916@hotmail.com>
2021-01-30 13:00:03 +0000

    hello

---
+++ src/components/CounterButton.js
@@ -0,0 +1,34 @@
+
+import React, {Component} from 'react'
+
+class CounterButton extends Component {
+	constructor() {
+		super()
+		this.state = {
+			count: 0
+		}
+	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+		console.log(nextProps, nextState)
+		return true
+	}
+
+	updateCount = () => {
+		this.setState(state => {
+			return {count: this.state.count + 1}
+		})
+	}
+
+	render() {
+		console.log('CounterButton')
+       	return (
+       		<button color={this.props.color} onClick={this.updateCount}>
+       			Counter: {this.state.count}
+       		</button>
+       	)
+	}
+	
+}
+
+export default CounterButton
